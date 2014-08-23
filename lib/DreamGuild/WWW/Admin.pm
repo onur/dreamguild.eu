53ff43650b17ea4dcf9a3e26ac6f466106a64203
Onur Aslan <onuraslan@gmail.com>
2014-08-23 12:53:44 +0300

    complete assign methods

--- lib/DreamGuild/WWW/Admin.pm
+++ lib/DreamGuild/WWW/Admin.pm
@@ -34,20 +34,90 @@ sub assign_account {
   my $uid = 0;
 
   DreamGuild::DB->iterate (
-    'SELECT id, name FROM roster ORDER BY name ASC',
+    'SELECT id, uid, name FROM roster ORDER BY name ASC',
     sub {
-      push @{$characters}, {
+      my $character = {
         id     => $_->[0],
-        name   => $_->[1],
+        uid    => $_->[1],
+        name   => $_->[2],
+        owner  => 0
       };
-      $uid = $_->[0] if ($_->[1] eq $account);
-      return 1;
+      $uid = $_->[1] if ($_->[2] eq $account);
+      push @{$characters}, $character;
     }
   );
 
-  warn "UID: $uid";
+  for (@{$characters}) {
+    $_->{owner} = 1 if ($uid == $_->{uid});
+  }
+
   $self->render (account => $account,
                  characters => $characters);
+}
+
+
+sub assign_account_post {
+  my $self = shift;
+  my $account = $self->param ('account');
+
+  my $user_id = 0;
+  my $current_main = 0;
+  my $new_main = $self->param ('main');
+  my @alts = $self->param ('alts');
+
+  my $success_message = '';
+
+  warn "MAIN $new_main";
+
+  # First select main account of $account (main character name)
+  DreamGuild::DB->iterate (
+    'SELECT uid FROM roster WHERE name = ?', $account,
+    sub {
+      $user_id = $_->[0]
+    }
+  );
+
+  return $self->render (template => 'error',
+                        error    => 'Unable to find user')
+      if (!$user_id);
+
+  my @alt_names;
+  my $new_main_name = '';
+  for (@alts) {
+    my $char = DreamGuild::DB::Roster->load ($_);
+
+    my $is_main = 0;
+
+    if ($char->{is_main}) {
+      $current_main = $char->{id};
+    }
+
+    if ($char->{id} == $new_main) {
+      $new_main_name = $char->{name};
+      $is_main = 1;
+    } elsif ($current_main == $new_main && $char->{name} eq $account) {
+      $is_main = 1;
+    }
+
+    $char->update (
+      uid => $user_id,
+      is_main => $is_main
+    );
+    push @alt_names, $char->{name};
+  }
+
+  
+  $success_message = "Successfully assigned: <br> $account => " . join (', ', @alt_names);
+
+  # If user chaged his main
+  if ($current_main != $new_main) {
+    my $user = DreamGuild::DB::User->load ($user_id);
+    $user->update (main => $new_main);
+    $success_message .= "<br>$account\'s new main changed to $new_main_name";
+  }
+
+  $self->flash ('text' => $success_message);
+  $self->redirect_to ('/admin/assign');
 }
 
 
