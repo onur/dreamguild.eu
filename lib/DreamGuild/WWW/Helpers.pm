cb1c14dc29a89a2764a774c7ea6c6439407e6c58
Onur Aslan <onuraslan@gmail.com>
2014-08-25 11:37:30 +0300

    fix relative time 's' usage

--- lib/DreamGuild/WWW/Helpers.pm
+++ lib/DreamGuild/WWW/Helpers.pm
@@ -18,11 +18,13 @@ sub register {
 
 
 
-my $sPerMinute = 60;
-my $sPerHour = $sPerMinute * 60;
-my $sPerDay = $sPerHour * 24;
-my $sPerMonth = $sPerDay * 30;
-my $sPerYear = $sPerDay * 365;
+my $timeUnits = [
+  [ 'year',   60 * 60 * 24 * 364 ],
+  [ 'month',  60 * 60 * 24 * 30  ],
+  [ 'day',    60 * 60 * 24       ],
+  [ 'hour',   60 * 60            ],
+  [ 'minute', 60                 ]
+];
 
 
 sub relative_date {
@@ -33,22 +35,18 @@ sub relative_date {
 
   my $elapsed = $current_time - $time;
 
-  if ($elapsed == 0) {
-    return "Just now";
-  } elsif ($elapsed < $sPerMinute) {
-    return "$elapsed seconds ago";
-  } elsif ($elapsed < $sPerHour) {
-    return int ($elapsed/$sPerMinute) . " minutes ago";
-  } elsif ($elapsed < $sPerDay) {
-    return int ($elapsed/$sPerHour) . " hours ago";
-  } elsif ($elapsed < $sPerMonth) {
-    return int ($elapsed/$sPerDay) . " days ago";
-  } elsif ($elapsed < $sPerYear) {
-    return int ($elapsed/$sPerMonth) . " months ago";
-  } else {
-    return int ($elapsed/$sPerYear) . " years ago";
+  return "Just now" if ($elapsed == 0);
+
+  my $ret = '';
+  for (@{$timeUnits}) {
+    if ($elapsed >= $_->[1]) {
+      my $num = int ($elapsed / $_->[1]);
+      $ret .= "$num $_->[0]" . ($num > 1 ? 's' : '') . ' ago';
+      last;
+    }
   }
 
+  return $ret;
 }
 
 
