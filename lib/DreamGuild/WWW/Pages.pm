25e606ea1adf02d9525de8e9d62c65c2e40945f1
Onur Aslan <onuraslan@gmail.com>
2014-08-26 12:53:12 +0300

    add edit and remove

--- lib/DreamGuild/WWW/Pages.pm
+++ lib/DreamGuild/WWW/Pages.pm
@@ -64,4 +64,82 @@ sub add_post {
 }
 
 
+sub admin_list {
+  my $self = shift;
+
+  my $pages = [];
+  DreamGuild::DB->iterate (
+    'SELECT slug, title FROM pages ORDER BY slug ASC',
+    sub {
+      push @{$pages}, [ $_->[0], $_->[1] ];
+    }
+  );
+
+
+  $self->render (pages => $pages);
+}
+
+
+sub edit {
+  my $self = shift;
+
+  my $page_row = DreamGuild::DB::Pages->select ('where slug = ?', $self->param ('slug'));
+  
+  return $self->render (status   => '404',
+                        template => 'error',
+                        error    => 'This page doesn\'t exist!')
+    unless (scalar (@{$page_row}));
+
+
+  $self->param (slug  => $page_row->[0]->{slug});
+  $self->param (title => $page_row->[0]->{title});
+  $self->param (content => $page_row->[0]->{content});
+
+  return $self->render (template => 'pages/add',
+                        page => $page_row->[0],
+                        js_ckeditor => 1);
+}
+
+
+sub edit_post {
+  my $self = shift;
+
+  my $page_row = DreamGuild::DB::Pages->select ('where slug = ?', $self->param ('slug'));
+  
+  return $self->render (status   => '404',
+                        template => 'error',
+                        error    => 'This page doesn\'t exist!')
+    unless (scalar (@{$page_row}));
+
+
+  my $slug = $self->param ('slug');
+  my $title = $self->param ('title');
+  my $content = $self->param ('content');
+
+  return $self->render (template => 'pages/add',
+                        error => 'All fields are required',
+                        js_ckeditor => 1)
+    if (!$slug || !$title || !$content);
+
+  $page_row->[0]->update (
+    'slug'    => $slug,
+    'title'   => $title,
+    'content' => $content
+  );
+
+  $self->flash (text => $title . ' successfully edited.');
+  return $self->redirect_to ('/admin/pages');
+}
+
+
+sub remove {
+  my $self = shift;
+
+  DreamGuild::DB::Pages->delete_where ('slug = ?', $self->param ('slug'));
+  $self->flash (text => $self->param ('slug') . ' successfully removed.');
+
+  return $self->redirect_to ('/admin/pages');
+}
+
+
 1;
