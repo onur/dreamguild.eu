3a1acfe6733af153cd886978b37ecf8438ec9d5b
Onur Aslan <onuraslan@gmail.com>
2014-12-21 14:08:28 +0200

    add mythic support to progress utility

--- lib/DreamGuild/Utils/Progress.pm
+++ lib/DreamGuild/Utils/Progress.pm
@@ -11,6 +11,9 @@ sub new {
 }
 
 
+# This function returns general progress of character progress
+# It's like 6/7 HC Highmaul, 14/14 HC SOO etc.
+# Designed to use in applications
 sub get_progress {
   my $self = shift;
   my $data = shift;
@@ -30,12 +33,14 @@ sub get_progress {
       my $flex_count = 0;
       my $normal_count = 0;
       my $heroic_count = 0;
+      my $mythic_count = 0;
 
       for my $boss (@{$raid->{bosses}}) {
         ++$lfr_count if ($boss->{lfrKills});
         ++$flex_count if ($boss->{flexKills});
         ++$normal_count if ($boss->{normalKills});
         ++$heroic_count if ($boss->{heroicKills});
+        ++$mythic_count if ($boss->{mythicKills});
       }
 
       push @{$progress}, { name       => $raid->{name},
@@ -43,7 +48,8 @@ sub get_progress {
                            lfr_count  => $lfr_count,
                            flex_count => $flex_count,
                            normal_count => $normal_count,
-                           heroic_count => $heroic_count };
+                           heroic_count => $heroic_count,
+                           mythic_count => $mythic_count };
 
     }
   }
